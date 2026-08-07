6ec566767f5c8ca9bdf3130c8951a06ddb105817
zmeetrajsinh04 <zmeetrajsinh04@gmail.com>
2026-08-07 10:59:48 +0530

    Add files via upload

---
+++ U3-3.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x;
+
+	clrscr();
+
+	printf("\n enter your age");
+	scanf("%d",&x);
+
+	if(x>=18)
+	{
+		printf("\n you are eligible for vote");
+	}
+	else
+	}
+
+
+		printf("\n you are not eligible for vote");
+
+	}
+	getch();
+}
