7a42b4d3f7a3e436bd3cd611e3560cc677056548
zmeetrajsinh04 <zmeetrajsinh04@gmail.com>
2026-09-24 14:43:30 +0530

    Add files via upload

---
+++ U3-EXTRA.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+ void main()
+{
+	int x,y,max;
+	clrscr();
+
+	printf("\n enter value of x:");
+	scanf("%d",&x);
+	printf("\n ente value of y:");
+	scanf("%d",&y);
+
+	(x>y) ? printf("x is max") : printf("y is max");
+
+
+	getch();
+
+}
+
+
+
+
+
+
